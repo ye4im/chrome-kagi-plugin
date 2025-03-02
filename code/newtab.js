0fcffa7507b309edb5b696147950343291913809
yexsi <63133923+yexsi@users.noreply.github.com>
2025-03-02 11:49:05 +0800

    add

--- code/newtab.js
+++ code/newtab.js
@@ -1,6 +1,6 @@
 // 获取存储的主页地址并重定向
 chrome.storage.sync.get(['homepage'], (data) => {
-  const defaultHomepage = 'https://kagi.com/';
+  const defaultHomepage = 'https://kagi.com/search?q=';
   const targetURL = data.homepage || defaultHomepage;
   window.location.replace(targetURL);
 });
